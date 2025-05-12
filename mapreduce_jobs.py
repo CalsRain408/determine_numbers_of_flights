60e85ed9c804a620e31cb78899be9a427ab68f0a
lemonseed <bv835113@student.reading.ac.uk>
2025-05-12 16:43:22 +0800

    MapReduce Jobs for Flight Data Analysis

--- mapreduce_jobs.py
+++ mapreduce_jobs.py
@@ -5,8 +5,6 @@ This module provides specific MapReduce jobs for analyzing flight data.
 """
 from typing import Dict, List, Any, Tuple, Iterable
 import logging
-from datetime import datetime
-from collections import defaultdict
 
 from mapreduce_framework import Mapper, Reducer, K, V, R
 
